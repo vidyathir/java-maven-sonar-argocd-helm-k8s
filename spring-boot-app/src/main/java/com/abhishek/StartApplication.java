30324fbf9975ad4542a4a9ee4526987548ba37fc
vidyathir <vidyathiruvenkadam@gmail.com>
2023-10-03 15:19:33 +0530

    adding restapi

--- spring-boot-app/src/main/java/com/abhishek/StartApplication.java
+++ spring-boot-app/src/main/java/com/abhishek/StartApplication.java
@@ -4,13 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 @Controller
+@RequestMapping("/demo")
 public class StartApplication {
 
-    @GetMapping("/")
+    @GetMapping("/data")
     public String index(final Model model) {
         model.addAttribute("title", "I have successfuly built a sprint boot application using Maven");
         model.addAttribute("msg", "This application is deployed on to Kubernetes using Argo CD");
